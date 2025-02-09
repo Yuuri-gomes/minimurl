d6eca08158aaab6037e81b1ad07ebae9aa0896c2
Yuuri-gomes <62670285+Yuuri-gomes@users.noreply.github.com>
2025-02-09 12:28:13 -0300

    style(favicon): update `favicon` at `attatchment-clipboard` icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
