ffa88b3ec9b2df878ebfa9dfa18e87fbd90cc8ed
Yuuri-gomes <62670285+Yuuri-gomes@users.noreply.github.com>
2025-02-10 14:02:26 -0300

    feat(adsense): set `seo` tools at `Minimurl` site

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8695005334801116"
+          crossorigin="anonymous"
+        ></script>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body>
